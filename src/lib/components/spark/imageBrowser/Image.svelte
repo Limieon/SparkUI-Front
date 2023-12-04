0743b0b069c1a7a37133924ce90f31fa913a8d52
Limieon <xxgmasterhdxx@gmail.com>
2023-12-04 01:23:25 +0100

    Reverted image hover animation

--- src/lib/components/spark/imageBrowser/Image.svelte
+++ src/lib/components/spark/imageBrowser/Image.svelte
@@ -2,6 +2,6 @@
 	export let url: string;
 </script>
 
-<button class="w-full h-full transition-transform hover:translate-y-2" on:click
+<button class="w-full h-full" on:click
 	><img class="block rounded bg-cover bg-center w-full aspect-square" src={url} alt="" />
 </button>
