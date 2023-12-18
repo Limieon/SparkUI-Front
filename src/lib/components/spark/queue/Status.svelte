e24be7ef3005756a84b1393e81587605eec84b2d
Limieon <justin.jakob391@outlook.com>
2023-12-18 01:35:35 +0000

    Updated colors for status

--- src/lib/components/spark/queue/Status.svelte
+++ src/lib/components/spark/queue/Status.svelte
@@ -9,15 +9,15 @@
 		<p>Queued</p>
 	</div>
 {:else if status === 'in_progress'}
-	<div class="bg-amber-600 rounded-xl w-fit pl-2 pr-3">
+	<div class="bg-amber-500 rounded-xl w-fit pl-2 pr-3">
 		<p>In Progress</p>
 	</div>
 {:else if status === 'completed'}
-	<div class="bg-green-700 rounded-xl w-fit pl-2 pr-3">
+	<div class="bg-emerald-600 rounded-xl w-fit pl-2 pr-3">
 		<p>Completed</p>
 	</div>
 {:else if status === 'cancelled'}
-	<div class="bg-red-700 rounded-xl w-fit pl-2 pr-3">
+	<div class="bg-rose-600 rounded-xl w-fit pl-2 pr-3">
 		<p>Cancelled</p>
 	</div>
 {/if}
