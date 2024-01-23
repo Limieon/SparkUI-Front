116e0a02c73c111c214f604f235abb0429578045
Justin Jakob <justin.jakob391@gmail.com>
2024-01-23 01:38:47 +0100

    Added type tooltip to Node connections

--- src/lib/components/spark/nodes/Node.svelte
+++ src/lib/components/spark/nodes/Node.svelte
@@ -4,6 +4,8 @@
 	import { Handle, Position, type NodeProps } from '@xyflow/svelte';
 	import type { Writable } from 'svelte/store';
 
+	import * as Tooltip from '$lib/components/ui/tooltip';
+
 	type $$Props = NodeProps;
 
 	export let data: {
@@ -19,35 +21,45 @@
 <div class="colorpicker bg-background rounded-xl w-fit">
 	<h2 class="text-lg font-bold bg-primary text-center rounded-t-xl pl-4 pr-4">{label}</h2>
 
-	<div class="p-2">
+	<div class="pl-2 pr-2 pb-2">
 		<div class="grid grid-cols-2">
 			<div>
 				{#each inputs as d, i}
 					<div class="flex items-center">
-						<span class="text-sm">{d.name ? d.name : getTypeName(d.type)}</span>
-						<Handle
-							id={i + 1 + ''}
-							type="target"
-							position={Position.Left}
-							style="top: calc(44px + (1.25rem * {i})); transform: translateX(-4px); background-color: {getTypeColor(
-								d.type
-							)}; width: 0.5rem; height: 0.5rem"
-						/>
+						<Tooltip.Root>
+							<Tooltip.Trigger
+								><Handle
+									id={i + 1 + ''}
+									type="target"
+									position={Position.Left}
+									style="top: calc(40px + (1.25rem * {i})); transform: translateX(-4px); background-color: {getTypeColor(
+										d.type
+									)}; width: 0.5rem; height: 0.5rem"
+								/>
+								<span class="text-sm">{d.name ? d.name : getTypeName(d.type)}</span>
+							</Tooltip.Trigger>
+							<Tooltip.Content class="bg-secondary">int</Tooltip.Content>
+						</Tooltip.Root>
 					</div>
 				{/each}
 			</div>
 			<div>
 				{#each outputs as d, i}
 					<div class="flex items-center float-right">
-						<span class="text-sm">{d.name ? d.name : getTypeName(d.type)}</span>
-						<Handle
-							id={i + 1 + inputs.length + ''}
-							type="source"
-							position={Position.Right}
-							style="top: calc(44px + (1.25rem * {i})); transform: translateX(4px); background-color: {getTypeColor(
-								d.type
-							)}; width: 0.5rem; height: 0.5rem"
-						/>
+						<Tooltip.Root>
+							<Tooltip.Trigger
+								><Handle
+									id={i + 1 + inputs.length + ''}
+									type="source"
+									position={Position.Right}
+									style="top: calc(38px + (1.25rem * {i})); transform: translateX(4px); background-color: {getTypeColor(
+										d.type
+									)}; width: 0.5rem; height: 0.5rem"
+								/>
+								<span class="text-sm">{d.name ? d.name : getTypeName(d.type)}</span>
+							</Tooltip.Trigger>
+							<Tooltip.Content class="bg-secondary">int</Tooltip.Content>
+						</Tooltip.Root>
 					</div>
 				{/each}
 			</div>
